4dc476789bb1fa09d01c371263f881dc3b65ee64
Arjun Thangaraju <axt7568@g.rit.edu>
2022-01-04 18:29:08 -0500

    Update training_script.py

--- training_script.py
+++ training_script.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#----------------------------------------------------------------------------
+# Created By  : Arjun Thangaraju
+# ---------------------------------------------------------------------------
 # Keras Time Series Anomaly Detection using HPV Data
 # Setup
 import math
